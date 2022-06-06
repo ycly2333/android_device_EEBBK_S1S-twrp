548c0406fbd0fa3583cbae1a12723920a7193331
ycly2333 <2780870183@qq.com>
2022-06-07 01:44:44 +0800

    twrp-7.1

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_S1S.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_S1S-user \
+    omni_S1S-userdebug \
+    omni_S1S-eng
---
+++ omni_S1S.mk
@@ -0,0 +1,9 @@
+# Inherit from H9S device
+$(call inherit-product, device/EEBBK/H9S/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := S1S
+PRODUCT_NAME := omni_S1S
+PRODUCT_BRAND := EEBBK
+PRODUCT_MODEL := S1 Pro
+PRODUCT_MANUFACTURER := EEBBK
